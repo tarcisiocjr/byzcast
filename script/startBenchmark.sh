6516af674d84aaa70d1d386fae27cc3de897c6e7
Tarcisio Ceolin Junior <tarcisio@ceolin.org>
2017-11-08 19:05:54 -0200

    Set some parameters for the tests

--- script/startBenchmark.sh
+++ script/startBenchmark.sh
@@ -6,13 +6,13 @@ cp -r /root/bftswarmGlobal/config config-global
 
 while IFS=$'\t' read hostname client_id local_group_id num_of_clients num_of_groups; do
 
-    case $hostname in
+    case ${hostname} in
         ''|\#*) continue ;; # skip blank lines and lines starting with #
     esac
 
 	if [ "$hostname" = "$HOSTNAME" ]; then
 	    echo "Starting benchmark"
-        java -cp '../lib/*:bin/*' ch.usi.dslab.ceolin.bftswarm.ClientThroughput $client_id $local_group_id $num_of_clients $num_of_groups "config-local" "config-global" false 120000
+        java -cp '../lib/*:bin/*' ch.usi.dslab.ceolin.bftswarm.ClientThroughput $client_id $local_group_id $num_of_clients $num_of_groups "config-local" "config-global" false 20000
 	fi
 
 done < /root/configBenchmark
