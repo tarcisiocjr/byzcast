5e38b73a7dec1e7b6c7bb581450d14c42197d7d4
Christian Vuerich <christian.vuerich@usi.ch>
2018-02-09 15:59:24 +0100

    created async-dev breanch and AsyncServer class, to experiment and understand byzcast. Objective is to remove auxiliary groups and have non blocking clients and server (async).

---
+++ src/main/java/ch/usi/inf/dslab/bftamcast/server/AsyncServer.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package ch.usi.inf.dslab.bftamcast.server;
+
+/**
+ * @author Christian Vuerich - christian.vuerich@usi.ch
+ *
+ * Temporary class to start experimenting and understanding byzcast
+ * 
+ * - understand byzcast structure
+ * - make server and clients non blocking (asynchronous)
+ * - remove auxiliary groups and use target groups to build the overlay tree
+ */
+public class AsyncServer {
+
+}
