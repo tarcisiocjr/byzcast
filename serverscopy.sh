43bb54f24a5108082537c0d8335ae26f00790f58
Christian Vuerich <christian.vuerich@usi.ch>
2018-03-22 22:18:29 +0100

    added new files to git

---
+++ serverscopy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [ $# -lt 1 ]; then
+    echo "$0 <GROUPS-#>"
+    exit 1
+fi
+
+for (( i = 0; i < $1; i++ )); do
+	# osascript -e 'tell app "Terminal"
+	# 	do script "echo " 
+	# end tell' $i
+
+	osascript -e 'tell application "Terminal" to do script "t;./groupcopy.sh '$i'"'
+	sleep 1
+done
