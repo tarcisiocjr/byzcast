80e62ee50c1a6463d36a09a438d31dea2386ba2a
Jefferson Oliveira <jeff_@MacBook-Air.local>
2022-04-23 02:34:02 -0300

    automate execution

---
+++ teste/run-client.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+IP=`ifconfig enp8s0d1 | grep "inet " | awk  '{print $2}'| awk '{print $1}'`
+
+if [ "$IP" = "" ]; then
+  IP=`ifconfig enp4s0 | grep "inet " | awk  '{print $2}'| awk '{print $1}'`
+fi
+
+if [ "$IP" = "" ]; then
+  IP=`ifconfig em1 | grep "inet " | awk  '{print $2}'| awk '{print $1}'`
+fi
+
+if [ "$IP" = "" ]; then
+  echo "No IP found, exiting!"
+  exit 1
+fi
+
+echo "Using IP '$IP'" 
+
+G_ID=`cat zones.txt | grep $IP |cut -f 5`
+
+LOCALGROUPS="-lcs "
+GLOBALGROUPS=""
+for i in `ls -d group*`; do
+  if [ "${i:0:7}" != "group-g" ]; then
+    LOCALGROUPS=$LOCALGROUPS"$i "
+  else
+    GLOBALGROUPS=$GLOBALGROUPS"$i "
+  fi
+done
+
+if [ "$LOCALGROUPS" = "-lcs " ]; then
+  echo "Single group configuration"
+  G_ID=0
+  LOCALGROUPS=""
+fi
+
+if [ "$G_ID" = "" ]; then
+  echo "Group id mismatch, exiting!"
+  exit
+fi
+
+echo "Running $QTY clients in group $G_ID..."
+java -cp '../target/*:../lib/*' ch.usi.inf.dslab.bftamcast.client.Client -i $RANDOM -g $G_ID -gc $GLOBALGROUPS $LOCALGROUPS $@
---
+++ teste/run-node.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+IP=`ifconfig enp8s0d1 | grep "inet " | awk  '{print $2}'| awk '{print $1}'`
+
+if [ "$IP" = "" ]; then
+  echo "No IP found, exiting!"
+  exit
+fi
+
+echo "Using IP '$IP'" 
+
+LOCALGROUPS=""
+GLOBALGROUPS=""
+for i in `ls -d group*`; do
+  echo $i
+  if [ "${i:0:7}" != "group-g" ]; then
+    LOCALGROUPS=$LOCALGROUPS"$i "
+  else
+    GLOBALGROUPS=$GLOBALGROUPS"$i "
+  fi
+
+  TEMP_N_ID=`cat $i/hosts.config |grep -w $IP| awk '{print $1}'`
+  if [ "$TEMP_N_ID" != "" ]; then
+    N_ID=$TEMP_N_ID
+    G_ID=`cat $i/hosts.config |grep "#group"| awk '{print $2}'`
+    echo "Folder $i, node id ${N_ID}, group id ${G_ID}"
+    #break
+  fi
+done
+
+if [ "$G_ID" = "" ] || [ "$N_ID" = "" ]; then
+  echo "Group/Node id mismatch, exiting!"
+  exit
+fi
+
+echo "LG=$LOCALGROUPS / GG=$GLOBALGROUPS $G_ID"
+if [ "${G_ID:0:1}" = "g" ]; then 
+    echo "GLOBAL SERVER"
+    echo "java -cp '../target/*:../lib/*' ch.usi.inf.dslab.bftamcast.server.BatchServerGlobal -i $N_ID -g ${G_ID:1:1} -gc $GLOBALGROUPS -lcs $LOCALGROUPS"
+    java -cp '../target/*:../lib/*' ch.usi.inf.dslab.bftamcast.server.BatchServerGlobal -i $N_ID -g ${G_ID:1:1} -gc $GLOBALGROUPS -lcs $LOCALGROUPS
+else
+    echo "LOCAL SERVER"
+    java -cp '../target/*:../lib/*' ch.usi.inf.dslab.bftamcast.server.Server -i $N_ID -g $G_ID -lc group-$G_ID
+fi
